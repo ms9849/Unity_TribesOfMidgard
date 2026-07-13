6f0af7a4c1dda21402c07d690bf12fda198fa0f7
ms9849 <hee26368@naver.com>
2026-07-13 14:45:40 +0900

    Notify UI 추가
    GameTimeManager의 OnDayStart/OnNightStart 이벤트에 맞춰 알림 텍스트를 표시하는 NotifyUI 스크립트를 추가하고 씬에 연결

---
+++ Assets/Scripts/UI/NotifyUI.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+// GameTimeManager의 낮/밤 전환 이벤트에 맞춰 알림 텍스트를 띄웁니다.
+public class NotifyUI : MonoBehaviour
+{
+    public static NotifyUI Instance { get; private set; }
+
+    [SerializeField] private float DisplayDuration = 3f;
+
+    private Text NotifyText;
+    private Coroutine HideRoutine;
+
+    void Awake()
+    {
+        Instance = this;
+        NotifyText = GetComponentInChildren<Text>(true);
+        NotifyText.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        if (GameTimeManager.Instance != null)
+        {
+            GameTimeManager.Instance.OnDayStart += HandleDayStart;
+            GameTimeManager.Instance.OnNightStart += HandleNightStart;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (GameTimeManager.Instance != null)
+        {
+            GameTimeManager.Instance.OnDayStart -= HandleDayStart;
+            GameTimeManager.Instance.OnNightStart -= HandleNightStart;
+        }
+    }
+
+    private void HandleDayStart()
+    {
+        Show("새 아침이 밝았습니다!");
+    }
+
+    private void HandleNightStart()
+    {
+        Show("밤이 찾아옵니다..");
+    }
+
+    public void Show(string message)
+    {
+        NotifyText.text = message;
+        NotifyText.gameObject.SetActive(true);
+
+        if (HideRoutine != null)
+            StopCoroutine(HideRoutine);
+        HideRoutine = StartCoroutine(HideAfterDelay());
+    }
+
+    private IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(DisplayDuration);
+        NotifyText.gameObject.SetActive(false);
+        HideRoutine = null;
+    }
+}
